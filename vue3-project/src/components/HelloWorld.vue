7ed7c7e273353bb35c0d9c27376a873479ad7198
Tang <745925880@qq.com>
2023-07-26 14:28:26 +0800

    111

--- vue3-project/src/components/HelloWorld.vue
+++ vue3-project/src/components/HelloWorld.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+const value=ref(0)
+
 </script>
 
 <template>
-<h3>vue3</h3>
+<h3 class="h3">{{ value }}</h3>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.h3{
+  color: $bg;
+}
 .read-the-docs {
   color: #888;
 }
--- vue3-project/vite.config.ts
+++ vue3-project/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, ConfigEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite"
 const pathSrc = path.resolve(__dirname, "src")
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
@@ -11,7 +13,31 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       },
     },
     plugins: [
-      vue()
-    ]
+      vue(),
+      //自动导导入api
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ["vue"],
+        eslintrc: {
+          enabled: false, // 是否自动生成 eslint 规则，建议生成之后设置 false 
+          filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
+        },
+        dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), // 指定自动导入函数TS类型声明文件路径
+      }),
+      //自动导入组件
+      Components({
+        dts: path.resolve(pathSrc, "types", "components.d.ts"),
+      })
+    ],
+    css: {
+      // CSS 预处理器
+      preprocessorOptions: {
+        //define global scss variable
+        scss: {
+          javascriptEnabled: true,
+          additionalData: `@use "@/styles/vari.scss" as *;`
+        }
+      }
+    },
   }
 })
