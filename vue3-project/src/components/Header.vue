eef9cb87478a83fd53bdf6598bd2bc3b9aeb3e0d
Tang <745925880@qq.com>
2023-07-26 12:03:35 +0800

    修改默认路径

---
+++ vue3-project/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script lang='ts'>
+import { defineComponent} from 'vue'
+export default defineComponent({
+
+})
+</script>
+
+
+<template>
+<div>111</div>
+</template>
+
+<style>
+</style>
--- vue3-project/vite.config.ts
+++ vue3-project/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ConfigEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
+const pathSrc = path.resolve(__dirname, "src")
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  return {
+    resolve: {
+      alias: {
+        "@": pathSrc,
+      },
+    },
+    plugins: [
+      vue()
+    ]
+  }
 })
